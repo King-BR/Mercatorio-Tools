9999b657457ea2e22429153f5d761b99b8288668
King-BR <fernandopach2017@gmail.com>
2024-10-10 20:46:00 -0300

    Add town_report route

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const port = 3000;
 // Serve static files (HTML, CSS, JS) from the 'public' folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// Route to handle /town_report
+app.get('/town_report', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'town_report.html'));
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
