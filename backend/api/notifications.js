01d4707ef73efe92bb09ae5153e11c294b20218e
King-BR <fernandopach2017@gmail.com>
2026-09-02 18:45:52 -0300

    fix importing error

--- backend/api/notifications.js
+++ backend/api/notifications.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const auth = require("../middleware/auth");
-const UsersDB = require("../models/users.js");
+const UsersDB = require("../models/user.js");
 const NotificationsDB = require("../models/notification.js");
 
 const router = express.Router();
