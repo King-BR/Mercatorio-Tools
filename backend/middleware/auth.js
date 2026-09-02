8de91e919800c6f69739692a61d550f973bfe0ce
King-BR <fernandopach2017@gmail.com>
2026-09-02 10:47:37 -0300

    Made auth middleware check for api keys too

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,17 +1,38 @@
 const jwt = require("jsonwebtoken");
+const Users = require("../models/user.js");
 
-module.exports = function (req, res, next) {
-  const token = req.cookies?.token || req.body?.token || req.query?.token || null;
+module.exports = async function (req, res, next) {
+  const token =
+    req.cookies?.token || req.body?.token || req.query?.token || null;
+  const bearerToken = req.headers?.authorization?.split(" ")[1] || null;
 
-  if (!token) {
-    return res.status(401).json({ message: "No token, authorization denied" });
+  if (!token && !bearerToken) {
+    return res
+      .status(401)
+      .json({ message: "No token or API key, authorization denied" });
   }
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user;
-    next();
-  } catch (err) {
-    res.status(401).json({ message: "Invalid token" });
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded.user;
+      next();
+    } catch (err) {
+      res.status(401).json({ message: "Invalid token" });
+    }
+  } else if (bearerToken) {
+    try {
+      const user = await Users.findOne({ "apiKeys.key": bearerToken });
+      if (!user) {
+        throw new Error("Invalid api key");
+      }
+
+      req.apiKey = bearerToken;
+      req.user = user;
+
+      next();
+    } catch (err) {
+      res.status(401).json({ message: "Invalid API key" });
+    }
   }
 };
