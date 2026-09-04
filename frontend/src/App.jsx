0639bdc9c038f41dd43b530384f4c9a5ad9a7f3e
King-BR <fernandopach2017@gmail.com>
2026-09-04 19:37:47 -0300

    Start work on account system

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+
 import Home from "./Pages/Home/Home";
 import ProductionPlanner from "./Pages/ProductionPlanner/ProductionPlanner";
+
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import Account from "./Pages/Account/Account";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/production-planner" element={<ProductionPlanner />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/production-planner" element={<ProductionPlanner />} />
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/register" element={<Register />} />
+
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
