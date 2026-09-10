1c7dca32313138b6f74713296eb937bb22f7b42f
King-BR <fernandopach2017@gmail.com>
2026-09-09 22:33:42 -0300

    add api call for market data

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -22,3 +22,13 @@ export async function getPrestigeSustenance() {
   const response = await fetch("/api/prestige/sustenance");
   return await response.json();
 }
+
+export async function getMarketData(town = "all") {
+  if (town == "all") {
+    const response = await fetch("/api/markets/all");
+    return await response.json();
+  }
+
+  const response = await fetch(`/api/markets/town/${town}`);
+  return await response.json();
+}
